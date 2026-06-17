a06b3ab3f44a4df790c75c701ff11c2a828def4f
SameenAdil <sameenkainaat1512@gmail.com>
2026-06-17 10:53:03 +0500

    setup.py done

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version= "0.0.1",
+    description= "it's a Wine Q package",
+    author= "SameenAdil",
+    packages= find_packages(),
+    license= "MIT"
+)
